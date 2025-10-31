dfc4c85d9c52bfdfe02e94f94686a90153db455c
simhadritejaswini6-droid <simhadritejaswini6@gmail.com>
2025-10-31 18:02:21 +0530

    Create negative.c

---
+++ negative.c
@@ -0,0 +1,14 @@
+//Displaying the given integer is a negative number//
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter an integer:");
+scanf("%d",&n);
+if(n<0)
+{
+printf("You entered %d.\n",n);
+}
+printf("the statement is negative");
+return 0;
+}
